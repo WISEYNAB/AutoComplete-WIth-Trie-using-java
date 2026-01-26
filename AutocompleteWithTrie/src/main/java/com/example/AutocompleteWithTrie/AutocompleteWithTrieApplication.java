a77aed7e68b19f1f1a3bc3fe66cddeed7d91c28e
WISEYNAB <109348369+WISEYNAB@users.noreply.github.com>
2026-01-26 22:11:57 +0530

    pure trie logic

---
+++ AutocompleteWithTrie/src/main/java/com/example/AutocompleteWithTrie/AutocompleteWithTrieApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutocompleteWithTrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteWithTrieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteWithTrieApplication.class, args);
+	}
+
+}
---
+++ AutocompleteWithTrie/src/test/java/com/example/AutocompleteWithTrie/AutocompleteWithTrieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutocompleteWithTrie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteWithTrieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
